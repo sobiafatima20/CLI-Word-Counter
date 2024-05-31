5a540c87d5507012610bafd64b1be31f2e03d234
Sobia Fatima <sobia.fatima2020@gmail.com>
2024-05-31 16:31:39 +0500

    word counter

---
+++ index.ts
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+
+//Import the 'inquirer' module, which is a command line interface for node.js
+import inquirer from 'inquirer';
+import chalk from 'chalk';
+
+//Declare a constant 'answer' and assign it to the result of inquirere.prompt function
+
+const answers:{
+    Sentence: string
+}= await inquirer.prompt([
+    {
+        name: "Sentence",
+        type: "input",
+        message: chalk.green("Enter your sentence to count the word: ")
+    }
+]);
+
+const words = answers.Sentence.trim().split(" ")
+
+//Print the array of words to the console
+console.log(words);
+
+//Print the word count of the sentence the the console
+console.log(chalk.gray(`Your sentence word count is ${words.length}`));
+
+
